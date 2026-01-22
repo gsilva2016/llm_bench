b914b8b2f6cd58685ea7b62828af5a86133d16c7
gsudo2020 <gsudo@none.com>
2026-01-21 17:08:54 -0700

    fix newline bug

--- main.py
+++ main.py
@@ -74,8 +74,10 @@ with open(bench_results_file, "w") as file:
             latency = (end-start) * 1000
             total_runs_t = total_runs_t + (end-start)
             total_t = total_t + latency
+
+            replaced_text = result.texts[0].replace('\n', '')
             
-            file.write(f"{num_questions}| {i+1}| {latency}| {quotes}{result.texts[0].replace('{newline}', '')}{quotes}| {result.perf_metrics.get_ttft().mean}| {result.perf_metrics.get_tpot().mean}| {result.perf_metrics.get_throughput().mean}|")
+            file.write(f"{num_questions}| {i+1}| {latency}| {quotes}{replaced_text}{quotes}| {result.perf_metrics.get_ttft().mean}| {result.perf_metrics.get_tpot().mean}| {result.perf_metrics.get_throughput().mean}|")
             if i < NUM_RUNS-1:
                 file.write("N/A|N/A\n")
             
